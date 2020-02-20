daaa7d7cf7ccb9740c627c3a983f7c54bacba6ca
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-02-20 16:00:47 +0900

    B:select_bothand()を作成

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.message.text
+    #message = event.message.text
+    message = select_bothand()
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=message))
